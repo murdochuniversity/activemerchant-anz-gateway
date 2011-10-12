df723b7e4549f9052879c1ecaa71fe8fea692251
Dirk Kelly <dk@dirkkelly.com>
2011-10-12 13:51:52 +0800

    initial commit of gem files

---
+++ lib/activemerchant-anz.rb
@@ -0,0 +1,7 @@
+require "activemerchant-anz/version"
+
+module Activemerchant
+  module Anz
+    # Your code goes here...
+  end
+end
---
+++ lib/activemerchant-anz/version.rb
@@ -0,0 +1,5 @@
+module Activemerchant
+  module Anz
+    VERSION = "0.0.1"
+  end
+end

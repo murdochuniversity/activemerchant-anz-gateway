00cc4670ce1cd8f3b9ab5507792d05fb6acb1a35
Dirk Kelly <dk@dirkkelly.com>
2011-10-12 15:33:50 +0800

    can't require from relative path in one nine

--- lib/active_merchant-anz.rb
+++ lib/active_merchant-anz.rb
@@ -1,6 +1,6 @@
 require "active_merchant"
-require "active_merchant/anz/version"
-require "active_merchant/billing/gateways/anz"
+require File.expand_path("#{__FILE__}../active_merchant/anz/version")
+require File.expand_path("#{__FILE__}../active_merchant/billing/gateways/anz")
 
 module ActiveMerchant
   module ANZ
